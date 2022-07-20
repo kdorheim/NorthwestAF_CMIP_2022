6ffc486fd1e40355b64cff712b452c86dcb4cca6
kdorheim <kalyn.dorheim@pnnl.gov>
2022-07-19 22:07:53 -0400

    materials

---
+++ R_example.R
@@ -0,0 +1,24 @@
+# R package that supports working with nc files
+library(ncdf4)
+
+# Establish the connection to the nc file
+nc <- nc_open("./tas_Amon_E3SM-1-0_historical_r1i1p1f1_gr_187501-189912.nc")
+
+# Extract the temperature data from the nc file
+tas_data <- ncvar_get(nc = nc, varid = "tas")
+
+# Extract the latitude to make the area weights
+lat <- ncvar_get(nc, "lat")
+lat_radian <- lat * 0.0174533
+weights <- cos(lat_radian)
+
+# Convert the vector of weights to a data frame to
+# be compatible with with the temperature data.
+weights_df <- matrix(rep(weights, 360), nrow = 360, byrow = FALSE)
+
+# Apply the weighted mean function to each time slice of data
+# to get the global average
+out <- apply(X = tas_data, MARGIN = 3, FUN = weighted.mean, w = weights_df)
+
+
+
---
+++ py_example.py
@@ -0,0 +1,18 @@
+# Packages
+import numpy as np
+import xarray as xr
+import fsspec
+
+# Load the data file
+ds = xr.open_zarr(fsspec.get_mapper("gs://cmip6/CMIP6/CMIP/E3SM-Project/E3SM-1-0/historical/r2i1p1f1/Amon/tas/gr/v20190729/"))
+
+# Extract the lat info to calculate the area weights
+lat = ds["lat"]
+weight = np.cos(np.deg2rad(lat))
+weight /= weight.mean()
+
+# The weighted average at each time slice
+other_dims = set(ds.dims) - {'time'}
+rslt = (ds * weight).mean(other_dims)
+
+
